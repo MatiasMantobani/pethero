3cd29d3a2261276bfecab142e07afb9420862212
ErreguerenaJM <66389899+ErreguerenaJM@users.noreply.github.com>
2022-09-30 14:57:29 -0300

    Dueno
    Agregar dueño
    Ver Dueños
    y un poco más

---
+++ Views/dueno-list.php
@@ -0,0 +1,50 @@
+<?php
+    require_once('nav.php');
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Listado de duenos</h2>
+               <table class="table bg-light-alpha">
+                    <thead>
+                         <th>Nombre</th>
+                         <th>Apellido</th>
+                         <th>DNI</th>
+                         <th>Direccion</th>
+                         <th>Telefono</th>
+                         <th>Email</th>
+                         <th>Contraseña</th>
+                         <th>Mascotas</th>
+                         <th>Reservas</th>
+                         <th>Pagos</th>
+
+                         </thead>
+                    <tbody>
+                         <?php
+                              foreach($duenoList as $dueno)
+                              {
+                                   ?>
+                                        <tr>
+                                             <td><?php echo $dueno->getFirstName() ?></td>
+                                             <td><?php echo $dueno->getLastName() ?></td>
+                                             <td><?php echo $dueno->getDNI() ?></td>
+                                             <td><?php echo $dueno->getAdress() ?></td>
+                                             <td><?php echo $dueno->getTelephone() ?></td>
+                                             <td><?php echo $dueno->getEmail() ?></td>
+                                             <td><?php echo $dueno->getPassword() ?></td>
+                                             <td><?php echo $dueno->getMascotas() ?></td>
+                                             <td><?php echo $dueno->getReservas() ?></td>
+                                             <td><?php echo $dueno->getPagos() ?></td>
+                                            
+
+                                             <!-- <td><?php //echo $dueno->getID() ?></td> -->
+                                        </tr>
+                                   <?php
+                              }
+                         ?>
+                         </tr>
+                    </tbody>
+               </table>
+          </div>
+     </section>
+</main>
