eacbf30c9b49b52d9a3060cfabcd0ef832acc772
MatiasMantobani <matiasmantobani1@gmail.com>
2022-10-03 13:07:37 -0300

    cambios horribles de git

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php namespace Config;
 define("ROOT", dirname(__DIR__) . "/");
-//define("FRONT_ROOT", "/proyectos/pethero/"); //Mati
- define("FRONT_ROOT", "/utn/pet-hero/"); // Rodri
+define("FRONT_ROOT", "/proyectos/pethero/"); //Mati
+//  define("FRONT_ROOT", "/utn/pet-hero/"); // Rodri
 // define("FRONT_ROOT", "/pethero/"); // Juan
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
