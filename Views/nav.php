be3720956593d2e5b96f29a8e7ea4b48475562e7
MatiasMantobani <matiasmantobani1@gmail.com>
2022-10-03 14:26:35 -0300

    navBar mejorada

--- Views/nav.php
+++ Views/nav.php
@@ -1,34 +1,56 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
 
     <a href="<?php echo FRONT_ROOT ?>">
-<img src="<?php echo FRONT_ROOT ?>Views/img/logo.png" height="65" alt="logo">
+        <img src="<?php echo FRONT_ROOT ?>Views/img/logo.png" height="65" alt="logo">
     </a>
 
-     <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto">
+
+        <!-- Admin -->
+        <li class="nav-item">
+            <!-- HECHO -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Dueno/ShowListView">Ver Todos Los Dueño</a>
+        </li>
+        <li class="nav-item">
+            <!-- HECHO -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Mascota/ShowListView">Ver Todas Las Mascotas</a>
+        </li>
+        <!-- HECHO -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Guardian/ShowListView">Ver Todos Los Guardianes</a>
+        </li>
+        <!--  -->
+
+        <!-- solo para dueño -->
+        <li class="nav-item">
+            <!-- VER COMO HACER PARA QUE LA MASCOTA CREADA SEA DEL DUEÑO -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>#">Crear Perfil Mascota</a>
+        </li>
+        <li class="nav-item">
+            <!-- VER COMO HACER PARA QUE VEA SOLO SUS MASCOTAS -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>#">Ver Mis Mascotas</a>
+        </li>
+        <!--  -->
 
 
-     <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Mascota/ShowAddView">Crear Perfil Mascota</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Mascota/ShowListView">Ver listado de Mascotas</a>
-          </li>
+        <!-- Solo para invitados/antes de loguearse -->
+        <li class="nav-item">
+            <!-- HECHO -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Person/LogInView">Log In</a>
+        </li>
+        <li class="nav-item">
+            <!-- HECHO -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Person/AddPerson">Crear Perfil</a>
+        </li>
+        <!--  -->
 
-         <li class="nav-item">
-             <a class="nav-link" href="<?php echo FRONT_ROOT ?>Person/LogInView">Log In</a>
-         </li>
+        <!-- Solo para guardian -->
+        <li class="nav-item">
+            <!-- HACER -->
+            <a class="nav-link" href="<?php echo FRONT_ROOT ?>#">Ver Mis Reservas</a>
+        </li>
+        <!-- Ver sus reservas (aceptarlas o no, estado de pago, bla) -->
+        <!--  -->
 
-
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Person/AddPerson">Crear Perfil</a>
-          </li>
-
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Guardian/ShowListView">Ver listado guardianes</a>
-          </li>
-
-         <li class="nav-item">
-             <a class="nav-link" href="<?php echo FRONT_ROOT ?>Dueno/ShowListView">Ver listado dueño</a>
-         </li>
-     </ul>
+    </ul>
 </nav>
