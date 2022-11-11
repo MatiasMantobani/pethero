c5630bcfe5ccf88df32d40bf72127b7e1f7fdea9
Rodrigo Guinazu <71780083+RodrigoGuinazu@users.noreply.github.com>
2022-11-11 03:06:57 -0300

    Mejorando la vista de perfil de 3ro y del chat

--- Views/chat.php
+++ Views/chat.php
@@ -1,127 +1,126 @@
 <?php
 require_once('nav.php');
 ?>
-<section style="background-color: #eee;">
-    <div class="container py-5">
+<br>
+<br>
+<div class="container py-5 bg-light-alpha rounded" >
 
-        <div class="row">
+    <div class="row">
 <!--caja de todas las conversaciones-->
-            <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
+        <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
 
-                <h5 class="font-weight-bold mb-3 text-center text-lg-start">Member</h5>
+            <h5 class="font-weight-bold mb-3 text-center text-lg-start">Member</h5>
 
-                <div class="card">
-                    <div class="card-body">
+            <div class="card">
+                <div class="card-body">
 
-                        <ul class="list-unstyled mb-0">
-                            <li class="p-2 border-bottom" style="background-color: #eee;">
-                                <a href="#!" class="d-flex justify-content-between">
-                                    <div class="d-flex flex-row">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar"
-                                             class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
-                                        <div class="pt-1">
-                                            <p class="fw-bold mb-0">John Doe</p>
-                                            <p class="small text-muted">Hello, Are you there?</p>
-                                        </div>
-                                    </div>
+                    <ul class="list-unstyled mb-0">
+                        <li class="p-2 border-bottom" style="background-color: #eee;">
+                            <a href="#!" class="d-flex justify-content-between">
+                                <div class="d-flex flex-row">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar"
+                                         class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                                     <div class="pt-1">
-                                        <p class="small text-muted mb-1">Just now</p>
-                                        <span class="badge bg-danger float-end">1</span>
+                                        <p class="fw-bold mb-0">John Doe</p>
+                                        <p class="small text-muted">Hello, Are you there?</p>
                                     </div>
-                                </a>
-                            </li>
+                                </div>
+                                <div class="pt-1">
+                                    <p class="small text-muted mb-1">Just now</p>
+                                    <span class="badge bg-danger float-end">1</span>
+                                </div>
+                            </a>
+                        </li>
 <!--                            chat seleccionado-->
-                            <li class="p-2 border-bottom">
-                                <a href="#!" class="d-flex justify-content-between">
-                                    <div class="d-flex flex-row">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
-                                             class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
-                                        <div class="pt-1">
-                                            <p class="fw-bold mb-0">Danny Smith</p>
-                                            <p class="small text-muted">Lorem ipsum dolor sit.</p>
-                                        </div>
-                                    </div>
+                        <li class="p-2 border-bottom">
+                            <a href="#!" class="d-flex justify-content-between">
+                                <div class="d-flex flex-row">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                                         class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                                     <div class="pt-1">
-                                        <p class="small text-muted mb-1">5 mins ago</p>
+                                        <p class="fw-bold mb-0">Danny Smith</p>
+                                        <p class="small text-muted">Lorem ipsum dolor sit.</p>
                                     </div>
-                                </a>
-                            </li>
+                                </div>
+                                <div class="pt-1">
+                                    <p class="small text-muted mb-1">5 mins ago</p>
+                                </div>
+                            </a>
+                        </li>
 <!--                            chat sin seleccionar-->
-                            <li class="p-2">
-                                <a href="#!" class="d-flex justify-content-between">
-                                    <div class="d-flex flex-row">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
-                                             class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
-                                        <div class="pt-1">
-                                            <p class="fw-bold mb-0">Brad Pitt</p>
-                                            <p class="small text-muted">Lorem ipsum dolor sit.</p>
-                                        </div>
-                                    </div>
+                        <li class="p-2">
+                            <a href="#!" class="d-flex justify-content-between">
+                                <div class="d-flex flex-row">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                                         class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                                     <div class="pt-1">
-                                        <p class="small text-muted mb-1">5 mins ago</p>
-                                        <span class="text-muted float-end"><i class="fas fa-check" aria-hidden="true"></i></span>
+                                        <p class="fw-bold mb-0">Brad Pitt</p>
+                                        <p class="small text-muted">Lorem ipsum dolor sit.</p>
                                     </div>
-                                </a>
-                            </li>
+                                </div>
+                                <div class="pt-1">
+                                    <p class="small text-muted mb-1">5 mins ago</p>
+                                    <span class="text-muted float-end"><i class="fas fa-check" aria-hidden="true"></i></span>
+                                </div>
+                            </a>
+                        </li>
 <!--                            ultimo chat de la lista (no tiene el border bottom)-->
-                        </ul>
+                    </ul>
 
-                    </div>
                 </div>
-
             </div>
+
+        </div>
 <!--caja del chat actual-->
-            <div class="col-md-6 col-lg-7 col-xl-8">
+        <div class="col-md-6 col-lg-7 col-xl-8">
 
-                <ul class="list-unstyled">
-                    <li class="d-flex justify-content-between mb-4">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
-                             class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
-                        <div class="card">
-                            <div class="card-header d-flex justify-content-between p-3">
-                                <p class="fw-bold mb-0">Brad Pitt</p>
-                                <p class="text-muted small mb-0"><i class="far fa-clock"></i> 12 mins ago</p>
-                            </div>
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                    labore et dolore magna aliqua.
-                                </p>
-                            </div>
+            <ul class="list-unstyled">
+                <li class="d-flex justify-content-between mb-4">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                         class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between p-3">
+                            <p class="fw-bold mb-0">Brad Pitt</p>
+                            <p class="text-muted small mb-0"><i class="far fa-clock"></i> 12 mins ago</p>
                         </div>
-                    </li>
+                        <div class="card-body">
+                            <p class="mb-0">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </li>
 <!--                    mensaje recibido-->
-                    <li class="d-flex justify-content-between mb-4">
-                        <div class="card w-100">
-                            <div class="card-header d-flex justify-content-between p-3">
-                                <p class="fw-bold mb-0">Lara Croft</p>
-                                <p class="text-muted small mb-0"><i class="far fa-clock"></i> 13 mins ago</p>
-                            </div>
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium.
-                                </p>
-                            </div>
+                <li class="d-flex justify-content-between mb-4">
+                    <div class="card w-100">
+                        <div class="card-header d-flex justify-content-between p-3">
+                            <p class="fw-bold mb-0">Lara Croft</p>
+                            <p class="text-muted small mb-0"><i class="far fa-clock"></i> 13 mins ago</p>
                         </div>
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
-                             class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
-                    </li>
+                        <div class="card-body">
+                            <p class="mb-0">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                laudantium.
+                            </p>
+                        </div>
+                    </div>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+                         class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
+                </li>
 <!--                    mensaje enviado-->
-                    <li class="bg-white mb-3">
-                        <div class="form-outline">
-                            <textarea class="form-control" id="textAreaExample2" rows="4"></textarea>
-                            <label class="form-label" for="textAreaExample2">Message</label>
-                        </div>
-                    </li>
+                <li class="bg-white mb-3">
+                    <div class="form-outline">
+                        <textarea class="form-control" id="textAreaExample2" rows="4" placeholder="Escribe un mensaje"></textarea>
+                    </div>
+                </li>
 <!--                    caja para escribir el mensaje-->
-                    <button type="button" class="btn btn-info btn-rounded float-end">Send</button>
+                <button type="button" class="btn btn-success">Send</button>
 <!--                    boton de enviar mensaje-->
-                </ul>
-
-            </div>
+            </ul>
 
         </div>
 
     </div>
-</section>
+
+</div>
--- Views/external-profile.php
+++ Views/external-profile.php
@@ -1,6 +1,13 @@
 <?php
 require_once('nav.php');
 ?>
+<header>
+    <!--    Sistema de rating starts-->
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/css/star-rating.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/js/star-rating.min.js"></script>
+</header>
 <main class="py-5">
     <section id="listado" class="mb-5">
         <div class="container">
@@ -43,12 +50,34 @@ require_once('nav.php');
 
                                 <hr class="mt-0 mb-4">
 
-<!--                                --><?php //if ($adress) { ?>
-                                    <h6>Direccion: </h6>
-                                    <p class="text-muted"><?php echo "Direccion" ?></p>
+                                <div class="row pt-1">
+                                    <div class="col-6 mb-3">
+                                        <!----><?php //if ($size) { ?>
+                                            <h6>Tamaños aceptados: </h6>
+                                            <p class="text-muted"><?php echo "Tamanos aceptados";?></p>
+                                        <!----><?php //} else { ?>
+                                            <!--<h6>Tamaños aceptados: </h6>-->
+                                            <!--<p class="text-muted">--><?php //echo "Todavia no indico que tamanos acepta";?><!--</p>-->
+                                        <!----><?php //} ?>
+                                    </div>
+                                </div>
+
+
+                                <hr class="mt-0 mb-4">
+
+<!--                                --><?php //if ($valoracion) { ?>
+                                    <h6>Valoracion (0): </h6>
+
+                                    <input id="rating" name="rating" class="rating" readonly showClear="false" showClear="false" value="4.5">
+
+<!--                                    <p class="text-muted">--><?php //echo "x" ?>
+<!--                                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="gold" color="gold" class="bi bi-star" viewBox="0 0 16 16">-->
+<!--                                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>-->
+<!--                                        </svg>-->
+<!--                                    </p>-->
 <!--                                --><?php //} else { ?>
-<!--                                    <h6>Direccion: </h6>-->
-<!--                                    <p class="text-muted">--><?php //echo "Sin direccion" ?><!--</p>-->
+<!--                                    <h6>Valoracion: </h6>-->
+<!--                                    <p class="text-muted">--><?php //echo "Sin valoracion" ?><!--</p>-->
 <!--                                --><?php //} ?>
 
                                 <hr class="mt-0 mb-4">
@@ -56,29 +85,14 @@ require_once('nav.php');
 
                                     <div class="row pt-1">
                                         <div class="col-6 mb-3">
-                                            <button type="button" class="btn btn-primary">
+                                            <a href="<?php echo FRONT_ROOT ?>Chat/ShowAddView/<?php  ?>" class="btn btn-success btn-lg">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
                                                     <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
                                                 </svg>
                                                 Enviar Mensaje
-                                            </button>
+                                            </a>
                                         </div>
                                     </div>
-
-                                    <hr class="mt-0 mb-4">
-
-                                    <div class="row pt-1">
-                                        <div class="col-6 mb-3">
-<!--                                            --><?php //if ($size) { ?>
-                                                <h6>Tamaños aceptados: </h6>
-                                                <p class="text-muted"><?php echo "Tamanos aceptados";?></p>
-<!--                                            --><?php //} else { ?>
-<!--                                                <h6>Tamaños aceptados: </h6>-->
-<!--                                                <p class="text-muted">--><?php //echo "Todavia no indico que tamanos acepta";?><!--</p>-->
-<!--                                            --><?php //} ?>
-                                        </div>
-                                    </div>
-
 
                                     <hr class="mt-0 mb-4">
                             </div>
