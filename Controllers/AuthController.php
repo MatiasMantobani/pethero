5c6b0894c1d28b1a4c835d288839b1a0e67826ed
MatiasMantobani <matiasmantobani1@gmail.com>
2022-11-13 14:44:32 -0300

    eror

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -10,12 +10,14 @@ use Controllers\HomeController as HomeController;
 
 class AuthController
 {
+
     private $userDao;
 
     public function __construct()
     {
         $this->userDao = new UserDAO();
     }
+
 
     public function Login($email, $password)
     {
@@ -48,12 +50,19 @@ class AuthController
 
         $controll = new HomeController();
         $controll->Index("Cierre de sesión correcto<br>");
-
     }
 
-    public function notFound(){
+    public function notFound()
+    {
         require_once(VIEWS_PATH . "404.php");
     }
 }
 
-?>
+/*
+try {
+} catch (Exception $ex) {
+// var_dump($ex);
+$_SESSION["message"] = "Error al ... De Autentificar";
+}
+
+*/
