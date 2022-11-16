d1288f7dfd4d637aacae342e2f01465209076df1
Rodrigo Guinazu <71780083+RodrigoGuinazu@users.noreply.github.com>
2022-11-16 01:04:54 -0300

    Agregando try catch a los metodos de SizeController

--- Controllers/SizeController.php
+++ Controllers/SizeController.php
@@ -23,52 +23,94 @@ class SizeController
 
     public function Add($small, $medium, $large)
     {
-        $size = new Size();
+        if ($this->validate()) {
+            try {
+                $size = new Size();
 
-        $size->setUserid($_SESSION['userid']);
-        $size->setSmall($small);
-        $size->setMedium($medium);
-        $size->setLarge($large);
+                $size->setUserid($_SESSION['userid']);
+                $size->setSmall($small);
+                $size->setMedium($medium);
+                $size->setLarge($large);
 
-        $this->sizeDAO->Add($size);
+                $this->sizeDAO->Add($size);
 
-        $_SESSION['message'][] = "Tamaño cargado con éxito";
-        $controller = new UserController();
-        $controller->ShowProfileView();
+                $_SESSION['message'][] = "Tamaño cargado con éxito";
+                $controller = new UserController();
+                $controller->ShowProfileView();
+            } catch (Exception $ex) {
+                HomeController::Index("Error al cargar los Tamaños aceptados");
+            }
+        }
     }
 
     public function Update($small, $medium, $large){
-        if ($small == false && $medium == false && $large == false){
-            $controller = new UserController();
-            $_SESSION['message'][] = "Error: Debe aceptar al menos un tamaño";
-            $controller->ShowProfileView();
-        } else {
-            if($this->SizeFinder($_SESSION['userid'])){
-                $this->sizeDAO->Remove($_SESSION['userid']);
-                $this->Add($small, $medium, $large);
-            } else {
-                $this->Add($small, $medium, $large);
+        if ($this->validate()) {
+            try {
+                if ($small == false && $medium == false && $large == false){
+                    $controller = new UserController();
+                    $_SESSION['message'][] = "Error: Debe aceptar al menos un tamaño";
+                    $controller->ShowProfileView();
+                } else {
+                    if($this->SizeFinder($_SESSION['userid'])){
+                        $this->sizeDAO->Remove($_SESSION['userid']);
+                        $this->Add($small, $medium, $large);
+                    } else {
+                        $this->Add($small, $medium, $large);
+                    }
+                }
+            } catch (Exception $ex) {
+                HomeController::Index("Error al modificar los Tamaños aceptados");
             }
         }
     }
 
     public function SizeFinder($userid){
-        return $this->sizeDAO->GetByUserid($userid);
+        if ($this->validate()) {
+            try {
+                return $this->sizeDAO->GetByUserid($userid);
+            } catch (Exception $ex) {
+                HomeController::Index("Error al encontrar el Tamaño");
+            }
+        }
     }
 
     public function getByUserId($userid){
-        $sizeDAO = new SizeDAO();
-        $size = $sizeDAO->getByUserId($userid);
-        if ($size){
-            return $size;
-        }else{
-            return null;
+        if ($this->validate()) {
+            try {
+                $sizeDAO = new SizeDAO();
+                $size = $sizeDAO->getByUserId($userid);
+                if ($size){
+                    return $size;
+                }else{
+                    return null;
+                }
+            } catch (Exception $ex) {
+                HomeController::Index("Error al traer los Tamaños aceptados por el guardian");
+            }
         }
     }
 
     public function Remove($userid){
-        $this->sizeDAO->Remove($userid);
+        if ($this->validate()) {
+            try {
+                $this->sizeDAO->Remove($userid);
+            } catch (Exception $ex) {
+                HomeController::Index("Error al eliminar el Tamaño");
+            }
+        }
     }
 
 }
 ?>
+
+/*
+
+if ($this->validate()) {
+try {
+
+} catch (Exception $ex) {
+HomeController::Index("Error al ... Tamaño");
+}
+}
+
+*/
