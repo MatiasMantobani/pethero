9be5235a94c80a675f4497dc85bb078268513928
MatiasMantobani <matiasmantobani1@gmail.com>
2022-11-16 12:12:43 -0300

    Update availableDate-add.php
    fechas pasadas ? imposible

--- Views/availableDate-add.php
+++ Views/availableDate-add.php
@@ -12,6 +12,7 @@
                     $(function() {
                         $('input[name="daterange"]').daterangepicker({
                             opens: 'left',
+                            minDate:new Date(),
                             locale: {
                                 format: "YYYY-MM-DD",
                                 separator: ','
@@ -21,6 +22,7 @@
                         });
                     });
                 </script>
+
 
                 <input type="text" name="daterange" />
                 <br>
